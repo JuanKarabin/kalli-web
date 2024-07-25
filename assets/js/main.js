2e4e2b07dd8e1ab039b2a8bd5812e21a4a01eaa2
jkarabin <jkarabin@frba.utn.edu.ar>
2024-07-25 02:01:31 -0300

    un poco de dinamismo

--- assets/js/main.js
+++ assets/js/main.js
@@ -181,3 +181,19 @@
   }
 
 })()
+
+document.addEventListener('DOMContentLoaded', function () {
+  const elements = document.querySelectorAll('.animate-on-scroll');
+
+  const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('visible');
+          }
+      });
+  });
+
+  elements.forEach(element => {
+      observer.observe(element);
+  });
+});
